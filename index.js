cb7821aa9556091b7f02c1de692ad4b9ececaf24
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-20 23:01:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+const session = require('express-session')
+const passport = require('passport')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const db = require('./db')
+const sessionStore = new SequelizeStore({db})
+const app = express()
+const socketio = require('socket.io')
+module.exports = app
+
+// This is a global Mocha hook, used for resource cleanup.
+// Otherwise, Mocha v4+ never quits after tests.
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id))
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findByPk(id) // Replace 'findById' with 'findByPk'
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'))
+
+  // body parsing middleware
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+
+  // compression middleware
+  app.use(compression())
+
+  // session middleware with passport
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false
+    })
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // auth and api routes
+  app.use('/auth', require('./auth'))
+  app.use('/api', require('./api'))
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname + '/public')));
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+  })
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+}
+
+const syncDb = () => db.sync()
+
+async function bootApp() {
+  await sessionStore.sync()
+  await syncDb()
+  await createApp()
+  await startListening()
+}
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  bootApp()
+} else {
+  createApp()
+}
