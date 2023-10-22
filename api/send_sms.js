60b5624fcabe3deb923a1ddba71613a177b05f9b
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-22 12:33:45 -0400

    Update send_sms.js

--- api/send_sms.js
+++ api/send_sms.js
@@ -1,12 +1,12 @@
 const client = require('twilio')(
-  'ACcb1b9e5e59f2e932b1c02a9cecd7d2b8',
-  '0c63dc2f45813538c8c5fcb3dd563ee8'
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
 )
 
 client.messages
   .create({
     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-    from: '+18482202516',
-    to: '+17328595701'
+    from: '+16475739491',
+    to: '+12568278087'
   })
   .then(message => console.log(message.sid))
