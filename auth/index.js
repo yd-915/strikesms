2c21b35d6db06f3604390c6233c8fd89cb64d6e6
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-22 12:25:32 -0400

    Update index.js

--- auth/index.js
+++ auth/index.js
@@ -4,8 +4,8 @@ module.exports = router
 
 const MessagingResponse = require('twilio').twiml.MessagingResponse
 const client = require('twilio')(
-  'ACcb1b9e5e59f2e932b1c02a9cecd7d2b8',
-  '0c63dc2f45813538c8c5fcb3dd563ee8'
+ process.env.TWILIO_ACCOUNT_SID,
+ process.env.TWILIO_AUTH_TOKEN
 )
 
 const twilioPhone = '+12568278087'
