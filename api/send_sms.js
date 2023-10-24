fe8afe886b3fbdb5c0a5f2f74058f8c9cadeb1f9
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-24 01:13:35 -0400

    Update send_sms.js

--- api/send_sms.js
+++ api/send_sms.js
@@ -5,8 +5,9 @@ const client = require('twilio')(
 
 client.messages
   .create({
-    body: 'Is this the end of fiat?',
-    from: '6474922775',
-    to: '6475739491'
+    body: '',
+    from: '+16474922775',
+    to: '+16475739491'
   })
   .then(message => console.log(message.sid))
+  .done();
