28c865c6620287b177fe89d6fc710c3b0de1c93c
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-01 01:19:12 -0400

    Update db.js

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    `postgresql://postgres:cCWUEtfMPrhQvoZtyrci@containers-us-west-118.railway.app:7447/railway`,
+    `postgresql://postgres:cCWUEtfMPrhQvoZtyrci@containers-us-west-118.railway.app:7447/railway`, 
   {
     logging: false
   }
