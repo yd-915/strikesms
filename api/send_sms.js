bcabfacd023618e48c3500ecf785db4f2c3ea344
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-05 01:01:28 -0400

    Update send_sms.js

--- api/send_sms.js
+++ api/send_sms.js
@@ -1,11 +1,10 @@
-const client = require('twilio')(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-)
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN';
+const client = require('twilio')(accountSid, authToken);
 
 client.messages
     .create({
-                to: '+16475739491'
+        to: '+16475739491'
     })
     .then(message => console.log(message.sid))
     .done();
